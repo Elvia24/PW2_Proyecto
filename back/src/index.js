24a277157fd2ffcaa449ed66e8044265b38ea531
BBasoria <159752827+BBasoria@users.noreply.github.com>
2024-02-18 22:33:54 -0600

    Update index.js
    solo agrege lo de mysql

--- back/src/index.js
+++ back/src/index.js
@@ -1,3 +1,6 @@
+//agregado de la clase, es todo lo que se modifico
+const mysql = require('mysql');
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -6,7 +9,15 @@ const cors = require('cors');
 app.use(cors());
 app.use('/',require('./routes/userRoutes'));
 
-
-
+//esta instrucción también estaba
+//app.use(express.json());
 
 app.listen(3000,()=>{console.log("server on port: 3000")});
+
+//agregado de la clase
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "ejemploreact"
+});
