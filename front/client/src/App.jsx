48a9885938b210d1e5b3abe52b599860ec298108
elvia24 <elviaarriaga24@gmail.com>
2024-04-01 05:21:20 -0600

    ordenando cositas
    arregle NotFound e Inicio

--- front/client/src/App.jsx
+++ front/client/src/App.jsx
@@ -1,5 +1,5 @@
 import {Route, Router, Routes} from 'react-router-dom';
-import inicio from './pages/inicio';
+import Inicio from './pages/inicio';
 import Registro from './pages/registro';
 import NotFound from './pages/NotFound';
 import InicioSesion from './pages/inicio-sesion';
@@ -7,9 +7,9 @@ import InicioSesion from './pages/inicio-sesion';
 function App(){
   return (
     <Routes>
-      <Route path='/' element={inicio} />
+      <Route path='/' element={<Inicio/>}  />
       <Route path='/ArtemiShop_Registro' element={<Registro/>} />
-      <Route path='*' element={NotFound}></Route>
+      <Route path='*' element={<NotFound/>}></Route>
       <Route path='/ArtemiShop_IniciarSesion' element={<InicioSesion />} />
     </Routes>
   )
--- front/client/src/pages/components/Navbar.jsx
+++ front/client/src/pages/components/Navbar.jsx
@@ -1,69 +1,71 @@
 // Navbar.jsx
-import React from 'react';
+import React, { useState } from 'react';
 
 function Navbar() {
-  return (
-    <div>
-          <div className="top-nav">
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <div>
+            <div className="top-nav">
                 <div className="container d-flex ">
-                
-                <ul className="d-flex">
-                <li><a href="/front/pages/inicio_sesion.html">Inicio Sesión</a></li>
-                <li><a href="/front/pages/registro.html">Registro</a></li>
-                
-                </ul>
-            </div>
-          </div>
-          <div className="navigation">
-            <div className="nav-center container d-flex">
-                <a href="../index.html" className="logo">
-                <div className="logo-container">
-                    <img src="../../src/images/Simbolo.png" alt="Logo de ArtemiShop" />
-                    <h1>Artemi<span className="shop">Shop</span></h1>
+
+                    <ul className="d-flex">
+                        <li><a href="/ArtemiShop_IniciarSesion">Inicio Sesión</a></li>
+                        <li><a href="/ArtemiShop_Registro">Registro</a></li>
+
+                    </ul>
                 </div>
-                </a>
-    
-                <ul className="nav-list d-flex">
-                <li className="nav-item">
-                    <a href="#" className="nav-link">Productos</a>
-                </li>
-                <li className="nav-item">
-                    <a href="#" className="nav-link">Categorias</a>
-                </li>
-                <li className="icons d-flex">
-                    <a href="../inicio_sesion.html" className="icon">
-                    <i className="bx bx-user"></i>
+            </div>
+            <div className="navigation">
+                <div className="nav-center container d-flex">
+                    <a href="/" className="logo">
+                        <div className="logo-container">
+                            <img src="../../src/images/Simbolo.png" alt="Logo de ArtemiShop" />
+                            <h1>Artemi<span className="shop">Shop</span></h1>
+                        </div>
                     </a>
-    
-                    <a href="../carrito.html" className="icon">
-                    <i className="bx bx-cart"></i>
-                    <span className="d-flex">0</span>
-                    </a>
-                </li>
-                </ul>
-    
-                <div className="icons d-flex">
-                <a href="../inicio_sesion.html" className="icon">
-                    <i className="bx bx-user"></i>
-                </a>
-                
-                <a href="../carrito.html" className="icon">
-                    <i className="bx bx-cart"></i>
-                    <span className="d-flex">0</span>
-                </a>
-                </div>
-    
-                <div className="hamburger">
-                <i className="bx bx-menu-alt-left"></i>
+                    {/* left sidebar*/}
+                    <ul className={`nav-list d-flex ${isOpen ? 'open' : ''}`}>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">Productos</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">Categorias</a>
+                        </li>
+                        <li className="icons d-flex">
+                            <a href="#" className="icon">
+                                <i className="bx bx-user"></i>
+
+                            </a>
+
+                            <a href="#" className="icon">
+                                <i className="bx bx-cart"></i>
+                                <span className="d-flex">0</span>
+                            </a>
+                        </li>
+                    </ul>
+                    {/* left sidebar*/}
+                    <div className="icons d-flex">
+                        <a href="#" className="icon">
+                            <i className="bx bx-user"></i>
+                        </a>
+
+                        <a href="#" className="icon">
+                            <i className="bx bx-cart"></i>
+                            <span className="d-flex">0</span>
+                        </a>
+                    </div>
+
+                    <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+                        <i className="bx bx-menu-alt-left"></i>
+                    </div>
                 </div>
             </div>
-            </div>
 
-    </div>
+        </div>
 
 
-    
-  );
+
+    );
 }
 
 export default Navbar;
